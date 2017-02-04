c43dee691202fbe3dfd2f6ff5dbcc9288b5ef55d
unQualsevol <kalagan@gmail.com>
2017-02-05 00:31:40 +0100

    add a rating bar in the details screen

--- app/src/main/java/com/unqualsevol/moviesproject1/DetailActivity.java
+++ app/src/main/java/com/unqualsevol/moviesproject1/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -24,6 +25,8 @@ public class DetailActivity extends AppCompatActivity {
 
     private TextView mUserRatingTextView;
 
+    private RatingBar mUserRatingRatingBar;
+
     private TextView mReleaseDateTextView;
 
     @Override
@@ -35,6 +38,7 @@ public class DetailActivity extends AppCompatActivity {
         mOriginalTitleTextView = (TextView) findViewById(R.id.tv_original_movie_title);
         mOverviewTextView = (TextView) findViewById(R.id.tv_movie_overview);
         mUserRatingTextView = (TextView) findViewById(R.id.tv_user_rating);
+        mUserRatingRatingBar = (RatingBar) findViewById(R.id.rb_movie_rating);
         mReleaseDateTextView = (TextView) findViewById(R.id.tv_release_date);
         Intent intentThatStartedThisActivity = getIntent();
 
@@ -49,10 +53,10 @@ public class DetailActivity extends AppCompatActivity {
                 .load(NetworkUtils.buildImageUrl(
                         getResources().getString(R.string.detail_size_poster), movie.getPosterPath()))
                 .into(mGreatPosterImageViem);
-        //TODO: show rating using stars
         mOriginalTitleTextView.setText(getString(R.string.detail_original_title) + movie.getOriginalTitle());
         mOverviewTextView.setText(getString(R.string.detail_synopsis) + movie.getOverview());
-        mUserRatingTextView.setText(getString(R.string.detail_user_rating) + movie.getVoteAverage());
+        mUserRatingRatingBar.setRating(movie.getVoteAverage().floatValue()/2);
+        mUserRatingTextView.setText("(" + movie.getVoteAverage() + ")");
         mReleaseDateTextView.setText(getString(R.string.detail_release_date) + movie.getReleaseDate());
     }
 }
