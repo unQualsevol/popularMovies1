4b05724d5a8726efd8012ad9aefb6a51474f0332
unQualsevol <kalagan@gmail.com>
2017-03-10 00:54:18 +0100

    unfavorite movies

--- app/src/main/java/com/unqualsevol/moviesproject1/model/ApplicationContract.java
+++ app/src/main/java/com/unqualsevol/moviesproject1/model/ApplicationContract.java
@@ -3,6 +3,7 @@ package com.unqualsevol.moviesproject1.model;
 public class ApplicationContract {
     public static final String INTENT_EXTRA_MOVIE_DATA = "movie";
     public static final String INTENT_EXTRA_MOVIE_ID = "movieId";
+    public static final String INTENT_EXTRA_FROM_DATABASE = "fromDatabase";
 
 
 }
